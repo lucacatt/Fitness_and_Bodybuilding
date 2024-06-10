2640b460841b26731434f4a394665c556d224f2c
Payd3r <payd3r2003@gmail.com>
2024-06-10 15:44:44 +0200

    Bottom Nav
    Implementazione della Bottom Navigation Bar e delle pagine del progetto

--- app/src/main/java/com/example/fitnessbodybuilding/MainActivity.kt
+++ app/src/main/java/com/example/fitnessbodybuilding/MainActivity.kt
@@ -5,20 +5,38 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.example.fitnessbodybuilding.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(300)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.btn1)
-        button.setOnClickListener {
-            val intent = Intent(this, Registrazione::class.java)
-            startActivity(intent)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView3.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.workout ->replaceFragment(Workout())
+                R.id.diary ->replaceFragment(Diary())
+                R.id.profile -> replaceFragment(Profile())
+                else->{
+                }
+            }
+            true
         }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction= fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
